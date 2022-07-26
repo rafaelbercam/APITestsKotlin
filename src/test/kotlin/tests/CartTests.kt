6ab13e93eb226371c4e6e50a5054fe9a6d15e95f
rafaelbercam <rbercam.medeiros@gmail.com>
2022-07-26 09:39:01 -0300

    add cart tests

--- src/test/kotlin/tests/CartTests.kt
+++ src/test/kotlin/tests/CartTests.kt
@@ -1,4 +1,36 @@
+@file:Suppress("ClassName")
 package tests
 
-class CartTests {
+import core.Setup
+import factory.LoginFactory
+import io.restassured.response.Response
+import org.apache.http.HttpStatus
+import org.junit.jupiter.api.*
+import requests.CartRequests
+import requests.LoginRequests
+import requests.ProductRequests
+import kotlin.test.assertEquals
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class CartTests: Setup() {
+
+    var login = LoginRequests()
+    var request = CartRequests()
+    lateinit var response: Response
+    var token : String = ""
+
+    @BeforeEach
+    fun `get token` () {
+        val user = LoginFactory()
+        response = login.login(user.loginSucceeded)
+        token = response.jsonPath().get("authorization")
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Listando todos carrinhos")
+    fun `list all products` (){
+        response = request.getCarts()
+        assertEquals(HttpStatus.SC_OK, response.statusCode())
+    }
 }
