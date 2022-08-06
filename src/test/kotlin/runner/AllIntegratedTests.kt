d83678cdca5d9d4df19ece40cd33167ea5f28f7f
rafaelbercam <rbercam.medeiros@gmail.com>
2022-08-06 12:09:39 -0300

    remove redundant test for class

--- src/test/kotlin/runner/AllIntegratedTests.kt
+++ src/test/kotlin/runner/AllIntegratedTests.kt
@@ -1,19 +1,8 @@
 package runner
 
-import org.junit.platform.suite.api.SelectClasses
 import org.junit.platform.suite.api.SelectPackages
 import org.junit.platform.suite.api.Suite
-import tests.CartTests
-import tests.LoginTests
-import tests.ProductTests
-import tests.UserTests
 
 @Suite
-@SelectClasses(
-    LoginTests::class,
-    UserTests::class,
-    ProductTests::class,
-    CartTests::class
-)
 @SelectPackages("tests")
 class AllIntegratedTests
